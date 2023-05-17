61316b6042d9180e3b330f520c62319abacc8fc3
Kevin Yang <3376630+kytozuk@users.noreply.github.com>
2023-05-17 13:12:57 +0900

    Convert all mysql2 field types to match planetscale's.

--- src/server.js
+++ src/server.js
@@ -100,6 +100,112 @@ async function handleQuery(sql = '') {
   return query(sql);
 }
 
+/**
+ * Test to match data types:
+ * 1. Create table locally and in planetscale
+ * 
+ *    create table data_type (
+ *       `decimal` decimal,
+ *       `tinyint` tinyint,
+ *       `tinyint_u` tinyint unsigned,
+ *       `smallint` smallint,
+ *       `smallint_u` smallint unsigned,
+ *       `int` int,
+ *       `int_u` int unsigned,
+ *       `float` float,
+ *       `double` double,
+ *       `timestamp` timestamp,
+ *       `bigint` bigint,
+ *       `bigint_u` bigint unsigned,
+ *       `mediumint` mediumint,
+ *       `mediumint_u` mediumint unsigned,
+ *       `date` date,
+ *       `time` time,
+ *       `datetime` datetime,
+ *       `year` year,
+ *       `varchar` varchar(10),
+ *       `bit` bit,
+ *       `json` json,
+ *       `enum` enum('a', 'b'),
+ *       `set` set('a', 'b'),
+ *       `tinyblob` tinyblob,
+ *       `tinytext` tinytext,
+ *       `mediumblob` mediumblob,
+ *       `mediumtext` mediumtext,
+ *       `longblob` longblob,
+ *       `longtext` longtext,
+ *       `blob` blob,
+ *       `varbinary` varbinary(10),
+ *       `char` char,
+ *       `binary` binary,
+ *       `geometry` geometry
+ *     );
+ * 
+ * 2. Run query for both databases
+ *     select * from data_type; 
+ * 
+ * 3. Match the 'types' field from the their results
+ */
+function extractType(type, flags) {
+  const {Types} = mysql;
+  // Refer to https://dev.mysql.com/doc/dev/mysql-server/latest/group__group__cs__column__definition__flags.html
+  // for the bitmask values
+  const isUnsigned = !!(flags & 32); 
+  const isBinary = !!(flags & 128);
+
+  // Convert mysql types to @planetscale/database:
+  // https://github.com/planetscale/database-js/blob/main/src/index.ts#L337
+  switch (type) {
+    case Types.TINY:    
+      return isUnsigned ? 'UINT8' : 'INT8';  
+    case Types.SHORT:
+      return isUnsigned ? 'UINT16' :  'INT16';
+    case Types.INT24:   
+      return isUnsigned ? 'UINT24' :  'INT24';
+    case Types.LONG:    
+      return isUnsigned ? 'UINT32' :  'INT32';
+    case Types.LONGLONG:
+      return isUnsigned ? 'UINT64' :  'INT64';
+    case Types.FLOAT:   
+      return 'FLOAT32';
+    case Types.DOUBLE: 
+      return 'FLOAT64';
+    case Types.VAR_STRING:
+      // varbinary, varchar
+      return isBinary ? 'VARBINARY' : 'VARCHAR';
+    case Types.STRING:
+      const isEnum = !!(flags & 256);
+      const isSet = !!(flags & 2048);
+      // enum, set, char, binary
+      return isEnum ? 'ENUM' : isSet ? 'SET' : isBinary ? 'BINARY' : 'CHAR';
+    case Types.NEWDECIMAL:
+      return 'DECIMAL';
+    case Types.BLOB:
+      // tinyblob, mediumblob, longblob, tinytext, mediumtext, longtext
+      return isBinary ? 'BLOB' : 'TEXT';
+  
+    default:
+      return Types[type];
+  }
+}
+
+function extractFields(fields = []) {
+  return fields.map(f => ({
+    name: f.name,
+    type: extractType(f.type, f.flags),
+    table: f.table,
+    orgTable: f.orgTable,
+    database: f.db,
+    orgName: f.orgName,
+    /**
+     * Disabled - not in use by @planetscale/database
+     */
+    // columnLength: '',
+    // charset: '',
+    // flags: f.flags,
+  }));
+}
+
 async function query(sql) {
   let error;
 
@@ -117,20 +223,7 @@ async function query(sql) {
         return resolve();
       }
 
-      const fields = _fields?.map(f => ({
-        name: f.name,
-        type: mysql.Types[f.type],
-        table: f.table,
-        orgTable: f.orgTable,
-        database: f.db,
-        orgName: f.orgName,
-        /**
-         * Disabled - not in use by @planetscale/database
-         */
-        // columnLength: '',
-        // charset: '',
-        // flags: f.flags,
-      })) || [];
+      const fields = extractFields(_fields);
 
       const rows = _results?.map ? _results.map(r => {
         const lengths = [];
